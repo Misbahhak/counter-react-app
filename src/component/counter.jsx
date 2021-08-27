e20ad0a34a18fe116f270d7b8f72c2a181aecd8e
Muhammad Misbah ul Haq <misbah.hak7@gmail.com>
2021-08-27 19:37:00 +0500

    React app done

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
+  // ******************** The Under function can be used for binding instead of  handleIncrement = () =>
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
   render() {
+    // console.log(this.props);  ** for prop children
     return (
-      <React.Fragment>
+      <div>
         {" "}
-        <span className={this.getbadgeclasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm"> Increment </button>{" "}
-      </React.Fragment>
+        <span className={this.getbadgeclasses()}> {this.formatCount()} </span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          {" "}
+          Increment{" "}
+        </button>{" "}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          {" "}
+          Delete
+        </button>
+      </div>
     );
   }
 
   getbadgeclasses() {
     let classes = "badge rounded-pill m2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
+
   formatCount() {
-    const { count } = this.state;
+    const count = this.props.counter.value;
     return count === 0 ? "zero" : count;
   }
 }
